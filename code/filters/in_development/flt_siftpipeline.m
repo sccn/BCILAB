d341b276d7155e72e9e545b9cfd1d322c2857281
Christian Kothe <christiankothe@gmail.com>
2014-02-12 03:55:49 -0800

    Reenabled some deprecated support for setting argsubtoggles to 0/false to turn them off. Fixed an issue with set_insert_markers inserting a marker at latency 0. Robustified set_selinterval to out-of-bounds markers. Added support for buffered continuous-data operation to flt_siftpipeline.

--- code/filters/in_development/flt_siftpipeline.m
+++ code/filters/in_development/flt_siftpipeline.m
@@ -1,4 +1,4 @@
-function [signal] = flt_siftpipeline(varargin)
+function [signal,state] = flt_siftpipeline(varargin)
 % Implements a basic SIFT pipeline 
 % (Preprocessing, Dynamical Modeling, Connectivity estimation)
 %
@@ -37,13 +37,15 @@ g = arg_define(varargin, ...
         arg_subtoggle({'collapseconn','CollapseConn'},'off',@hlp_collapseConn,'Collapse connectivity across time or freq'), ...
         arg_subtoggle({'validation','Validation'},{'EEG',signal},@est_validateMVAR,'Validate Model fit'), ...
         arg({'printValidation','PrintValidation'},false,[],'Print validation output to console'), ...
-        arg({'verb','VerbosityLevel'},0,{int32(0) int32(1) int32(2)},'Verbosity level. 0 = no output, 1 = text, 2 = graphical') ...
+        arg({'verb','VerbosityLevel'},0,{int32(0) int32(1) int32(2)},'Verbosity level. 0 = no output, 1 = text, 2 = graphical'), ...
+        arg_nogui({'state','State'}) ...
         );
 
 if g.selModelOrder.arg_selection && length(g.selModelOrder.modelOrderSelection.icselector) > 1
     error('SIFT:onl_siftpipeline:MoreThanOneIC','Only one InformationCriterion can be selected for ModelOrderSelection.'); end
             
-signal = g.signal; 
+state = g.state;
+preproc = g.signal;
 g      = rmfield(g,'signal');
 
 % Implement basic SIFT pipeline (Preprocessing, Modeling, Connectivity)
@@ -51,74 +53,116 @@ g      = rmfield(g,'signal');
 
 % select channels
 % -------------------------------------------------------------------------
-if ~isempty(g.channels) && ~isequal(g.channels,1:signal.nbchan) && ~isequal(g.channels,{char(zeros(1,0))}) % TODO: remove last isequal hack
-    if g.verb
-        signal = pop_select(signal,'channel',g.channels,'sorttrial','off');
+if ~isempty(g.channels) && ~isequal(g.channels,1:preproc.nbchan) && ~isequal(g.channels,{char(zeros(1,0))}) % TODO: remove last isequal hack
+    preproc.chanlocs = preproc.chanlocs(g.channels);
+    preproc.data = preproc.data(g.channels,:,:);
+    preproc.nbchan = size(preproc.data,1);
+end
+
+
+% buffer data if not epoched
+% -------------------------------------------------------------------------
+if isempty(state)
+    for f = utl_timeseries_fields(signal)
+        state.(f{1}).buffer = zeros(size(signal.(f{1}),1),0);
+        state.(f{1}).next_index = 1;
+    end
+end
+
+if isempty(preproc.epoch)
+    winstep = max(1,round(g.modeling.winstep * preproc.srate));
+    winlen = max(1,round(g.modeling.winlen * preproc.srate));
+    % get the field that we want to buffer
+    srcFieldMap = struct('Channels','data','Sources','srcpot','Components','icaact');    
+    f = {srcFieldMap.(g.preproc.sigtype.arg_selection)};
+    % prepend buffer contents to data
+    preproc.(f{1}) = [state.(f{1}).buffer preproc.(f{1})]; 
+    % determine the correct window positions for the desired source signal
+    g.modeling.winStartIdx = state.(f{1}).next_index:winstep:(size(preproc.(f{1}),2)-winlen+1);
+    g.modeling.winStartIdx(g.modeling.winStartIdx<1) = [];
+    if ~isempty(g.modeling.winStartIdx)
+        % buffer all data that will be needed on the next call
+        next_index = max(g.modeling.winStartIdx)+winstep;
+        state.(f{1}).buffer = preproc.(f{1})(:,next_index:end);
+        % decrement next_index based on how much we at the beginning
+        state.(f{1}).next_index = next_index - (size(preproc.(f{1}),2) - size(state.(f{1}).buffer,2));
     else
-        [console_text,signal] = evalc('pop_select(signal,''channel'',g.channels,''sorttrial'',''off'');');  %#ok<ASGLU>
+        % keep everything buffered
+        state.(f{1}).buffer = preproc.(f{1});
     end
+    preproc.pnts = size(preproc.data,2);
 end
 
-% pre-process data
-% -------------------------------------------------------------------------
-signal = pre_prepData('EEG',signal,g.preproc,'verb',g.verb,'arg_direct',true);
-                
-% get the m-file name of the function implementing the modeling approach
-% -------------------------------------------------------------------------
-modelingFuncName = hlp_getModelingApproaches('mfileNameOnly',g.modeling.arg_selection);
-
-% determine optimal model order
-% -------------------------------------------------------------------------
-if g.selModelOrder.arg_selection
-    g.selModelOrder.modelOrderSelection.modelingApproach = g.modeling;
-    IC = est_selModelOrder('EEG',signal,g.selModelOrder.modelOrderSelection,'arg_direct',true);
-    if iscell(IC), IC = IC{1}; end
-    icselector = g.selModelOrder.modelOrderSelection.icselector{1};
-    switch g.selModelOrder.minimizer.arg_selection
-        case 'min'
-            popt = ceil(mean(IC.(icselector).popt));                    
-        case 'elbow'
-            popt = ceil(mean(IC.(icselector).pelbow));
-        case 'prctile'
-            popt = ceil(prctile(IC.(icselector).popt,g.selModelOrder.minimizer.prclim));
-    end
-    g.modeling.morder = popt;
-end
-            
-% fit model
-% -------------------------------------------------------------------------
-signal.data(~isfinite(signal.data(:))) = 0;
-signal.CAT.MODEL = feval(modelingFuncName,'EEG',signal,g.modeling,'verb',g.verb,'arg_direct',true);
-
-% perform model validation
-% -------------------------------------------------------------------------
-if g.validation.arg_selection
-    [whitestats PCstats stability residualstats] = est_validateMVAR('EEG',signal,g.validation,'arg_direct',true);
-    if isempty(whitestats)
-        whitenessCriteria = {};
-    else
-        whitenessCriteria = g.validation.checkWhiteness.whitenessCriteria;
-    end
-    if g.printValidation
-        vis_validation(whitestats,PCstats,stability,whitenessCriteria);
-    end
-    % store results
-    signal.CAT.validation.whitestats = whitestats;
-    signal.CAT.validation.PCstats    = PCstats;
-    signal.CAT.validation.stability  = stability;
-    signal.CAT.validation.residualstats = residualstats;
-end
-
-% calculate connectivity
-% -------------------------------------------------------------------------
-if g.connectivity.arg_selection
-    signal.CAT.Conn = est_mvarConnectivity('EEG',signal,'MODEL',signal.CAT.MODEL,g.connectivity,'verb',g.verb,'arg_direct',true);
-    if g.collapseconn.arg_selection
-        [signal.CAT.Conn, signal.CAT.Conn.peak_freqs] = hlp_collapseConn('Conn',signal.CAT.Conn,g.collapseconn,'arg_direct',true);
-    end
+if isempty(preproc.epoch) && isempty(g.modeling.winStartIdx)
+    % data is empty, return empty model
+    % -------------------------------------------------------------------------
+    signal.CAT = hlp_sift_emptyset;
+    signal.CAT.Conn = hlp_sift_emptyconn;
+    signal.CAT.MODEL = hlp_sift_emptymodel;
 else
-    signal.CAT.Conn = [];
-end
 
+    % pre-process data
+    % -------------------------------------------------------------------------
+    preproc = pre_prepData('EEG',preproc,g.preproc,'verb',g.verb,'arg_direct',true);
+
+    % get the m-file name of the function implementing the modeling approach
+    % -------------------------------------------------------------------------
+    modelingFuncName = hlp_getModelingApproaches('mfileNameOnly',g.modeling.arg_selection);
+
+    % determine optimal model order
+    % -------------------------------------------------------------------------
+    if g.selModelOrder.arg_selection
+        g.selModelOrder.modelOrderSelection.modelingApproach = g.modeling;
+        IC = est_selModelOrder('EEG',preproc,g.selModelOrder.modelOrderSelection,'arg_direct',true);
+        if iscell(IC), IC = IC{1}; end
+        icselector = g.selModelOrder.modelOrderSelection.icselector{1};
+        switch g.selModelOrder.minimizer.arg_selection
+            case 'min'
+                popt = ceil(mean(IC.(icselector).popt));                    
+            case 'elbow'
+                popt = ceil(mean(IC.(icselector).pelbow));
+            case 'prctile'
+                popt = ceil(prctile(IC.(icselector).popt,g.selModelOrder.minimizer.prclim));
+        end
+        g.modeling.morder = popt;
+    end
+
+    % fit model
+    % -------------------------------------------------------------------------
+    preproc.data(~isfinite(preproc.data(:))) = 0;
+    preproc.CAT.MODEL = feval(modelingFuncName,'EEG',preproc,g.modeling,'verb',g.verb,'arg_direct',true);
+
+    % perform model validation
+    % -------------------------------------------------------------------------
+    if g.validation.arg_selection
+        [whitestats PCstats stability residualstats] = est_validateMVAR('EEG',preproc,g.validation,'arg_direct',true);
+        if isempty(whitestats)
+            whitenessCriteria = {};
+        else
+            whitenessCriteria = g.validation.checkWhiteness.whitenessCriteria;
+        end
+        if g.printValidation
+            vis_validation(whitestats,PCstats,stability,whitenessCriteria);
+        end
+        % store results
+        preproc.CAT.validation.whitestats = whitestats;
+        preproc.CAT.validation.PCstats    = PCstats;
+        preproc.CAT.validation.stability  = stability;
+        preproc.CAT.validation.residualstats = residualstats;
+    end
+
+    % calculate connectivity
+    % -------------------------------------------------------------------------
+    if g.connectivity.arg_selection
+        preproc.CAT.Conn = est_mvarConnectivity('EEG',preproc,'MODEL',preproc.CAT.MODEL,g.connectivity,'verb',g.verb,'arg_direct',true);
+        if g.collapseconn.arg_selection
+            [preproc.CAT.Conn, preproc.CAT.Conn.peak_freqs] = hlp_collapseConn('Conn',preproc.CAT.Conn,g.collapseconn,'arg_direct',true);
+        end
+    else
+        preproc.CAT.Conn = [];
+    end
+
+    signal.CAT = preproc.CAT;
+end
 
 exp_endfun;
